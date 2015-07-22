471d0916ad902c1ef64345a863f15043bc72cda0
Ryan B. Harvey <ryan_b_harvey@omb.eop.gov>
2015-07-22 16:03:47 -0400

    Prepared build scripts and HTML files with appropriate relative paths for release to WH.gov.

--- buildtools/build.sh
+++ buildtools/build.sh
@@ -5,8 +5,14 @@ rm -rf ../dist/*
 
 # Create dist directory
 mkdir ../dist
-mkdir ../dist/assets
-mkdir ../dist/assets/images
+mkdir ../dist/omb
+mkdir ../dist/omb/place
+mkdir ../dist/sites
+mkdir ../dist/sites/default
+mkdir ../dist/sites/default/files
+mkdir ../dist/sites/default/files/omb_place
+mkdir ../dist/sites/default/files/omb_place/assets
+mkdir ../dist/sites/default/files/omb_place/assets/images
 
 # Minify/Optimize JS
 ./optimize-js.sh
@@ -15,18 +21,18 @@ mkdir ../dist/assets/images
 ./optimize-css.sh
 
 # Copy HTML files + favicon
-cp ../index-minified.html ../dist/index.html
-cp ../print-minified.html ../dist/print.html
-cp ../datasets-minified.html ../dist/datasets.html
+cp ../index-minified.html ../dist/omb/place/index.html
+cp ../print-minified.html ../dist/omb/place/print.html
+cp ../datasets-minified.html ../dist/omb/place/datasets.html
 cp ../favicon.ico ../dist/
 
 # Link data directory
-ln -fhs ../data ../dist/data
+ln -fhs ../data ../dist/sites/default/files/omb_place/data
 
 # Copy image and font assets
-cp -r ../assets/leaflet-0.7.3/css/images ../dist/assets/
-cp -r ../assets/images/* ../dist/assets/images/
-cp -r ../assets/fonts ../dist/assets/
+cp -r ../assets/leaflet-0.7.3/css/images ../dist/sites/default/files/omb_place/assets/
+cp -r ../assets/images/* ../dist/sites/default/files/omb_place/assets/images/
+cp -r ../assets/fonts ../dist/sites/default/files/omb_place/assets/
 
 # Modify html JS and CSS references?
 
